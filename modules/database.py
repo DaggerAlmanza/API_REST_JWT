fb267706e483f7b5d7bfd39cf3347a7c399c7841
DaggerAlmanza <85371943+DaggerAlmanza@users.noreply.github.com>
2021-10-14 09:27:50 -0500

    Actualizacion y separación: 1. View, 2. Processors, 3. DB

--- modules/database.py
+++ modules/database.py
@@ -1,21 +1,7 @@
-import os
-
-from api_json_web_token import app
-from flask_migrate import Migrate
-from flask_sqlalchemy import SQLAlchemy
+from api import db
 
 
-directorio = os.path.abspath(os.path.dirname(__file__))
-app.config['SECRET_KEY'] = 'Dramyson1024'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' +\
-                            os.path.join(directorio, 'todo.sqlite')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
-Migrate(app, db)
-
-
-class Usuario(db.Model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     public_id = db.Column(db.String(50), unique=True)
     name = db.Column(db.String(50))
@@ -23,7 +9,7 @@ class Usuario(db.Model):
     admin = db.Column(db.Boolean)
 
 
-class Todo(db.Model):
+class SuperUser(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(50))
     complete = db.Column(db.Boolean)
